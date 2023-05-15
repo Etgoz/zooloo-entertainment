93deb06c0073af1381c3d44b0b61a6bd2e888107
Etgoz <etgoz151@gmail.com>
2023-05-15 12:59:11 +0300

    initial init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Header } from './components/Header';
+
+export function App() {
+	return (
+		<div className='container-fluid '>
+			<Header />
+		</div>
+	);
+}
